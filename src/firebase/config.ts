e33f4c63e0d84dcfd1650e061c77cab11263fc7e
Genti Selenica <genti@atworkapp.co>
2026-02-11 15:51:29 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2978195510-c91bc",
+  "appId": "1:523347283892:web:8e0231e6fb82ab5766ace1",
+  "apiKey": "AIzaSyA4DzgxbjW0Qlf3HQvp_nwTH129HFfk_qc",
+  "authDomain": "studio-2978195510-c91bc.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "523347283892"
+};
