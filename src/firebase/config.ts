972ce4416351339c0e27c7accda4b874256b9f12
Hello Marigo <hello@marigoapp.com>
2026-02-17 14:18:20 +0000

    connect with this project in firebase
    // Import the functions you need

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-2978195510-c91bc",
-  "appId": "1:523347283892:web:8e0231e6fb82ab5766ace1",
-  "apiKey": "AIzaSyA4DzgxbjW0Qlf3HQvp_nwTH129HFfk_qc",
-  "authDomain": "studio-2978195510-c91bc.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "523347283892"
+  "apiKey": "AIzaSyAj6mdkO-0sVr0UUETRSuv3WvTnfuW31O4",
+  "authDomain": "marigoappcom-v10-6377709-d8775.firebaseapp.com",
+  "projectId": "marigoappcom-v10-6377709-d8775",
+  "storageBucket": "marigoappcom-v10-6377709-d8775.appspot.com",
+  "messagingSenderId": "329665870351",
+  "appId": "1:329665870351:web:ba1b91bbe4b6704b50817a"
 };
